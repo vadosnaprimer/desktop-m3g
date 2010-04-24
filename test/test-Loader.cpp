39d547a62b9c05010d70c515120f115369ac3c0b
tueda <tueda@wolf.dog.cx>
2010-04-24 07:46:10 +0000

    微修正

--- test/test-Loader.cpp
+++ test/test-Loader.cpp
@@ -9,14 +9,12 @@ TEST (Loader_load)
 {
   std::vector<Object3D*> objs = Loader::load ("simple.m3g");
 
-  CHECK_EQUAL (14, objs.size());
+  CHECK_EQUAL (12, objs.size());
 
   // 注意：M3Gの仕様ではオブジェクトインデックスは１から始まる。
   // 0番はダミー(=0)、１番はヘッダーオブジェクト(=0),
   // 使用するのは２番から
-  CHECK_EQUAL ((Object3D*)0, objs[0]);
-  CHECK_EQUAL ((Object3D*)0, objs[1]);
-  CHECK (objs[2] != NULL);
+  CHECK (objs[0] != NULL);
   
 }
 
