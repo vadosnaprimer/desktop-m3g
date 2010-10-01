38c3698d857f6105318b3a41c3615342701b6abc
tueda <tueda@wolf.dog.cx>
2010-10-01 07:26:53 +0000

    微修正

--- sample/test-15-animation-translate/main.cpp
+++ sample/test-15-animation-translate/main.cpp
@@ -12,159 +12,164 @@ World* wld = 0;
 
 void display(void)
 {
-  Graphics3D* g3d = Graphics3D::getInstance();
-  g3d->render (wld);
-  glutSwapBuffers();
+    Graphics3D* g3d = Graphics3D::getInstance();
+    g3d->render (wld);
+    glutSwapBuffers();
 }
 
 void resize(int w, int h)
 {
-  Graphics3D* g3d = Graphics3D::getInstance();
-  g3d->setViewport (0,0,w,h);
-  Camera* cam = wld->getActiveCamera();
-  cam->setPerspective (45, w/(float)h, 0.1, 100);
+    Graphics3D* g3d = Graphics3D::getInstance();
+    g3d->setViewport (0,0,w,h);
+    Camera* cam = wld->getActiveCamera();
+    cam->setPerspective (45, w/(float)h, 0.1, 100);
 }
 
 void quit ()
 {
-  for (int i = 0; i < (int)objs.size(); i++) {
-    delete objs[i];
-  }
-  Graphics3D* g3d = Graphics3D::getInstance();
-  delete g3d;
-  exit (0);
+    for (int i = 0; i < (int)objs.size(); i++) {
+        delete objs[i];
+    }
+    Graphics3D* g3d = Graphics3D::getInstance();
+    delete g3d;
+    exit (0);
 }
 
 int world_time = 0;
 
 static void keyboard(unsigned char key, int x, int y)
 {
-  switch (key) {
-  case 'q':
-  case 'Q':
-  case '\033':
-    quit ();
+    switch (key) {
+    case 'q':
+    case 'Q':
+    case '\033':
+        quit ();
     break;
-  case ' ':
-    cout << "glut: Space, time = " << world_time << "\n";
-    wld->animate (world_time);
-    world_time += 2;
-  default:
-    break;
-  }
-  glutPostRedisplay();
+    case ' ':
+        cout << "glut: Space, time = " << world_time << "\n";
+        wld->animate (world_time);
+        world_time += 2;
+    default:
+        break;
+    }
+    glutPostRedisplay();
 }
 
 
 int main (int argc, char** argv)
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-  glutCreateWindow(argv[0]);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+    glutCreateWindow(argv[0]);
 
-  AnimationController* controller_translation = new AnimationController;
-  //controller->setActiveInterval (0, 1000);
-  controller_translation->setSpeed (1, 0);
-  AnimationController* controller_orientation = new AnimationController;
-  controller_orientation->setSpeed (2, 0);
+    AnimationController* controller_translation = new AnimationController;
+    //controller->setActiveInterval (0, 1000);
+    controller_translation->setSpeed (1, 0);
+    AnimationController* controller_orientation = new AnimationController;
+    controller_orientation->setSpeed (2, 0);
   
-  Quaternion q0 (0, 0,1,0);
-  Quaternion q1 (180, 0,1,0);
-  Quaternion q2 (360, 0,1,0);
-  cout << "q0 = " << q0 << "\n";
-  cout << "q1 = " << q1 << "\n";
-  cout << "q2 = " << q2 << "\n";
-  KeyframeSequence* keyframe_mesh_translate = new KeyframeSequence (4, 3, KeyframeSequence::LINEAR);
-  KeyframeSequence* keyframe_mesh_orientation = new KeyframeSequence (3, 4, KeyframeSequence::SLERP);
-  float frame_mesh_translate[3][3]= {{0,0,0},
-                                     {4,0,0},
-                                     {-4,0,0}};
-  float frame_mesh_orientation[3][4]= {{q0.x, q0.y, q0.z, q0.w},
-                                     {q1.x, q1.y, q1.z, q1.w},
-                                     {q2.x, q2.y, q2.z, q2.w}};
-  keyframe_mesh_translate->setKeyframe (0, 0,   frame_mesh_translate[0]);
-  keyframe_mesh_translate->setKeyframe (1, 100, frame_mesh_translate[1]);
-  keyframe_mesh_translate->setKeyframe (2, 300, frame_mesh_translate[2]);
-  keyframe_mesh_translate->setKeyframe (3, 400, frame_mesh_translate[0]);
-  keyframe_mesh_translate->setRepeatMode (KeyframeSequence::LOOP);
-  keyframe_mesh_translate->setValidRange (0, 3);
-  keyframe_mesh_translate->setDuration (400);
-  keyframe_mesh_orientation->setKeyframe (0, 0,   frame_mesh_orientation[0]);
-  keyframe_mesh_orientation->setKeyframe (1, 100, frame_mesh_orientation[1]);
-  keyframe_mesh_orientation->setKeyframe (2, 200, frame_mesh_orientation[2]);
-  keyframe_mesh_orientation->setRepeatMode (KeyframeSequence::LOOP);
-  keyframe_mesh_orientation->setValidRange (0, 2);
-  keyframe_mesh_orientation->setDuration (200);
+    //        x,y,z, w  or angle, axis(x,y,z)
+    // q0 = [ 0,0,0, 1  or 0 ,0,0,0 ]
+    // q1 = [ 0,1,0, 0  or 180 ,0,1,0 ]
+    // q2 = [ 0,0,0, -1 or 360 ,0,1,0 ]
 
-  AnimationTrack* animation_mesh_translation = new AnimationTrack (keyframe_mesh_translate, AnimationTrack::TRANSLATION);
-  animation_mesh_translation->setController (controller_translation);
-  AnimationTrack* animation_mesh_orientation = new AnimationTrack (keyframe_mesh_orientation, AnimationTrack::ORIENTATION);
-  animation_mesh_orientation->setController (controller_orientation);
+    //Quaternion q0 (0, 0,1,0);
+    //Quaternion q1 (180, 0,1,0);
+    //Quaternion q2 (360, 0,1,0);
+    //cout << "q0 = " << q0 << "\n";
+    //cout << "q1 = " << q1 << "\n";
+    //cout << "q2 = " << q2 << "\n";
+    KeyframeSequence* keyframe_mesh_translate   = new KeyframeSequence (4, 3, KeyframeSequence::LINEAR);
+    KeyframeSequence* keyframe_mesh_orientation = new KeyframeSequence (3, 4, KeyframeSequence::SLERP);
+    float frame_mesh_translate[3][3]= {{0,0,0},
+                                       {4,0,0},
+                                       {-4,0,0}};
+    float frame_mesh_orientation[3][4]= {{0,0,0, 1},
+                                         {0,1,0, 0},
+                                         {0,0,0, -1}};
+    keyframe_mesh_translate->setKeyframe (0, 0,   frame_mesh_translate[0]);
+    keyframe_mesh_translate->setKeyframe (1, 100, frame_mesh_translate[1]);
+    keyframe_mesh_translate->setKeyframe (2, 300, frame_mesh_translate[2]);
+    keyframe_mesh_translate->setKeyframe (3, 400, frame_mesh_translate[0]);
+    keyframe_mesh_translate->setRepeatMode (KeyframeSequence::LOOP);
+    keyframe_mesh_translate->setValidRange (0, 3);
+    keyframe_mesh_translate->setDuration (400);
+    keyframe_mesh_orientation->setKeyframe (0, 0,   frame_mesh_orientation[0]);
+    keyframe_mesh_orientation->setKeyframe (1, 100, frame_mesh_orientation[1]);
+    keyframe_mesh_orientation->setKeyframe (2, 200, frame_mesh_orientation[2]);
+    keyframe_mesh_orientation->setRepeatMode (KeyframeSequence::LOOP);
+    keyframe_mesh_orientation->setValidRange (0, 2);
+    keyframe_mesh_orientation->setDuration (200);
 
-  Background* bg = new Background;
-  bg->setColor (0xff7f7f7f);
+    AnimationTrack* animation_mesh_translation = new AnimationTrack (keyframe_mesh_translate, AnimationTrack::TRANSLATION);
+    animation_mesh_translation->setController (controller_translation);
+    AnimationTrack* animation_mesh_orientation = new AnimationTrack (keyframe_mesh_orientation, AnimationTrack::ORIENTATION);
+    animation_mesh_orientation->setController (controller_orientation);
 
-  Camera* cam = new Camera;
-  cam->lookAt (0,0,10,
-               0,0,0,
-               0,1,0);
+    Background* bg = new Background;
+    bg->setColor (0xff7f7f7f);
 
-  VertexArray* positions         = new VertexArray (4, 3, 2);
-  short        position_values[] = {1,-1,0, 1,1,0, -1,-1,0, -1,1,0};
-  positions->set (0, 4, position_values);
+    Camera* cam = new Camera;
+    cam->lookAt (0,0,10,
+                 0,0,0,
+                 0,1,0);
 
-  float scale = 1;
-  float bias[3] = {0,0,0};
-  VertexBuffer* vertices = new VertexBuffer;
-  vertices->setPositions (positions, scale, bias);
+    VertexArray* positions         = new VertexArray (4, 3, 2);
+    short        position_values[] = {1,-1,0, 1,1,0, -1,-1,0, -1,1,0};
+    positions->set (0, 4, position_values);
 
-  int strips[1]  = {4};
-  int indices[4] = {0,1,2,3};
-  TriangleStripArray* tris = new TriangleStripArray (indices, 1, strips);
+    float scale = 1;
+    float bias[3] = {0,0,0};
+    VertexBuffer* vertices = new VertexBuffer;
+    vertices->setPositions (positions, scale, bias);
 
-  Material* mat = new Material;
-  mat->setColor (Material::DIFFUSE, 0xff0000ff);
+    int strips[1]  = {4};
+    int indices[4] = {0,1,2,3};
+    TriangleStripArray* tris = new TriangleStripArray (indices, 1, strips);
 
-  PolygonMode* pmode = new PolygonMode;
-  pmode->setCulling (PolygonMode::CULL_NONE);
+    Material* mat = new Material;
+    mat->setColor (Material::DIFFUSE, 0xff0000ff);
 
-  Appearance* app = new Appearance;
-  app->setMaterial (mat);
-  app->setPolygonMode (pmode);
+    PolygonMode* pmode = new PolygonMode;
+    pmode->setCulling (PolygonMode::CULL_NONE);
 
-  Mesh* mesh = new Mesh (vertices, tris, app);
-  mesh->addAnimationTrack (animation_mesh_translation);
-  mesh->addAnimationTrack (animation_mesh_orientation);
+    Appearance* app = new Appearance;
+    app->setMaterial (mat);
+    app->setPolygonMode (pmode);
 
-  wld = new World;
-  wld->addChild (cam);
-  wld->setActiveCamera (cam);
-  wld->setBackground (bg);
-  wld->addChild (mesh);
+    Mesh* mesh = new Mesh (vertices, tris, app);
+    mesh->addAnimationTrack (animation_mesh_translation);
+    mesh->addAnimationTrack (animation_mesh_orientation);
 
-  cout << *wld << "\n";
+    wld = new World;
+    wld->addChild (cam);
+    wld->setActiveCamera (cam);
+    wld->setBackground (bg);
+    wld->addChild (mesh);
 
-  objs.push_back (controller_translation);
-  objs.push_back (controller_orientation);
-  objs.push_back (keyframe_mesh_translate);
-  objs.push_back (keyframe_mesh_orientation);
-  objs.push_back (animation_mesh_translation);
-  objs.push_back (animation_mesh_orientation);
-  objs.push_back (bg);
-  objs.push_back (cam);
-  objs.push_back (positions);
-  objs.push_back (vertices);
-  objs.push_back (tris);
-  objs.push_back (mat);
-  objs.push_back (pmode);
-  objs.push_back (app);
-  objs.push_back (mesh);
-  objs.push_back (wld);
+    cout << *wld << "\n";
 
-  glutKeyboardFunc(keyboard);
-  glutDisplayFunc(display);
-  glutReshapeFunc(resize);
-  glutMainLoop ();
+    objs.push_back (controller_translation);
+    objs.push_back (controller_orientation);
+    objs.push_back (keyframe_mesh_translate);
+    objs.push_back (keyframe_mesh_orientation);
+    objs.push_back (animation_mesh_translation);
+    objs.push_back (animation_mesh_orientation);
+    objs.push_back (bg);
+    objs.push_back (cam);
+    objs.push_back (positions);
+    objs.push_back (vertices);
+    objs.push_back (tris);
+    objs.push_back (mat);
+    objs.push_back (pmode);
+    objs.push_back (app);
+    objs.push_back (mesh);
+    objs.push_back (wld);
 
-  return 0;
+    glutKeyboardFunc(keyboard);
+    glutDisplayFunc(display);
+    glutReshapeFunc(resize);
+    glutMainLoop ();
+
+    return 0;
 }
